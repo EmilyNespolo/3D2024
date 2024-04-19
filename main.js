eff3786ce9a16c1f64ad0efc11304b5eaa7a6416
Emily Nespolo <106550910+EmilyNespolo@users.noreply.github.com>
2024-04-19 08:55:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ for (let i = 0; i < botoes.length; i++) {
 
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
+            abas-textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
+        abas-textos[i].classList.add("ativo");
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
